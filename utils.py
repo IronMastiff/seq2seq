ebba0b64b5e623e601b5e14ba16ce526407a6c43
Iron Mastiff <1760298283@qq.com>
2018-02-23 23:09:15 +0800

    Created batch_data function and finished utils

--- utils.py
+++ utils.py
@@ -16,5 +16,21 @@ def extract_vocab( data ):
 
     return int_to_vocab, vocab_to_int
 
-def pad_id_sequences( source_ids, source_vocab_to_int, target_ids, target_vocab_to_int, sequence_lengh ):
-    new_source_ids = [list( reversed( sentence + [source_vocab_to_int['<pad>']] * ( sequence_lengh - len( sentence ) ) ))]
+def pad_id_sequences( source_ids, source_vocab_to_int, target_ids, target_vocab_to_int, sequence_length ):
+    new_source_ids = [list( reversed( sentence + [source_vocab_to_int['<pad>']] * ( sequence_lengh - len( sentence ) ) ) )
+                      for sentence in source_ids]
+    new_target_ids = [ sentence + [target_vocab_to_int['<pad>']] * ( sequence_length - len( sentence ) )
+                       for sentence in target_ids]
+
+    return new_source_ids, new_target_ids
+
+def batch_data( source, target, batch_size ):
+    """
+    Batch source and target together
+    """
+    for batch_i in range( 0, len( sources ) // batch_size ):
+        start_i = batch_i * batch_size
+        source_batch = source[start_i : start_i + batch_size]
+        target_batch = target[start_i : start_i + batch_size]
+
+        yield source_batch, target_batch
