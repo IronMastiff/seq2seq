8e1c3fba03f6f405b9cbf70abb0e43719ae64a95
Iron Mastiff <1760298283@qq.com>
2018-02-24 18:31:26 +0800

    created extract_character_vocab

--- seq2seq.py
+++ seq2seq.py
@@ -6,7 +6,18 @@ import utils
 source_dir = './data/letters_source.txt'
 target_dir = './data/letters_target.txt'
 
+'''--------Load data--------'''
 source_sentences = utils.load_data( source_dir )
 target_sentences = utils.load_data( target_dir )
 
-print( source_sentences[: 50].split( '\n' ) )
+'''--------Preprocess--------'''
+def extract_character_vocab( data ):
+    special_words = ['<PAD>', '<UNK>', '<GO>', '<EOS>']
+
+    set_words = set( [character for line in data.split( '\n' ) for character in line] )
+    int_to_vocab = {word_i : word for word_i, word in enumerate( special_words + list( set_words ) )}
+    vocab_to_int = { word_i : word for word_i, word in int_to_vocab.items()}
+
+    return int_to_vocab, vocab_to_int
+
+# Build int2letter and letter2int dictis
